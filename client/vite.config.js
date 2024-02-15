13e97b287e6f52edd32726d3c718874b5bbcb18e
Wolffkran <liebherr2008@live.com>
2024-02-15 02:47:44 -0500

    wow

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   server: {
     port: 3000,
@@ -11,8 +12,13 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:3001',
         secure: false,
-        changeOrigin: true
-      }
-    }
-  }
-})
+        changeOrigin: true,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '/src': path.resolve(__dirname, 'src/main.jsx'), 
+    },
+  },
+});
